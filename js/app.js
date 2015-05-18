c24a1b39942ebf0db6d0082d62e2c75fa81cf472
archlicher <christophergbbg@yahoo.com>
2015-05-18 21:45:58 +0300

    welcome-screen is home-screen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('socialNetwork', []);
+'use strict';
+
+var socialNetwork = angular.module('SocialNetwork', ['ngRoute']);
+
+socialNetwork.constant('baseUrl', 'http://softuni-social-network.azurewebsites.net/api/');
+
+socialNetwork.config(function($routeProvider) {
+	$routeProvider
+		.when('#/',{
+			templateUrl:'partials/home-screen.html'
+		})
+});
